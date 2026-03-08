87c79cd0e3f998a1ff9504356c41287fac45fe32
tigranayv2010-lang <tigranayv2010@gmail.com>
2026-03-08 11:02:05 +0400

    Update back_button.py

--- handlers/back_button.py
+++ handlers/back_button.py
@@ -1,10 +1,7 @@
-from utils import get_user_data, load_json, reset_stack
+from utils import get_user_data, reset_stack
 from messages import MESSAGES
-from keyboards import main_menu_keyboard, wallet_menu_keyboard, back_button
-
-# Импортируем словарь стадий чтобы очищать при нажатии «Назад»
+from keyboards import main_menu_keyboard, wallet_menu_keyboard, topup_currency_keyboard, back_button
 import handlers.create_deal as create_deal_module
-import handlers.details as details_module
 
 
 def _show_main_menu(bot, call, lang):
@@ -26,6 +23,15 @@ def _show_details(bot, call, cid, lang):
     )
 
 
+def _show_topup(bot, call, cid):
+    bot.edit_message_text(
+        text="💰 Пополнение баланса\n\nВыберите валюту:",
+        chat_id=cid,
+        message_id=call.message.message_id,
+        reply_markup=topup_currency_keyboard()
+    )
+
+
 def register_back_button_handler(bot):
 
     @bot.callback_query_handler(func=lambda call: call.data.startswith("back_to_"))
@@ -33,9 +39,8 @@ def register_back_button_handler(bot):
         cid = call.message.chat.id
         target = call.data.replace("back_to_", "")
 
-        # Отменяем активную сделку если пользователь нажал «Назад»
+        # Отменяем активную сделку и очищаем next_step_handler
         create_deal_module.creating_deal_stage.pop(cid, None)
-        # Очищаем next_step_handler чтобы он не сработал на следующее сообщение
         bot.clear_step_handler_by_chat_id(cid)
 
         user_data = get_user_data(cid)
@@ -46,6 +51,16 @@ def register_back_button_handler(bot):
             _show_main_menu(bot, call, lang)
         elif target == "details":
             _show_details(bot, call, cid, lang)
+        elif target == "topup":
+            _show_topup(bot, call, cid)
+        elif target == "withdraw":
+            from keyboards import withdraw_currency_keyboard
+            bot.edit_message_text(
+                text="📤 Вывод средств\n\nВыберите валюту для вывода:",
+                chat_id=cid,
+                message_id=call.message.message_id,
+                reply_markup=withdraw_currency_keyboard()
+            )
         else:
             reset_stack(cid)
             _show_main_menu(bot, call, lang)
